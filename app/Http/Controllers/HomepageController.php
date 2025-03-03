9888761b2ca4f6eaf4e6c84adda7c0791eccc335
Abid Raza <ari.abid@gmail.com>
2025-03-04 00:55:10 +0530

    Send latestPosts data on homepage

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
 use App\Models\Course;
 use App\Models\Page;
 use App\Models\Post;
@@ -11,14 +12,16 @@ use Inertia\Inertia;
 
 class HomepageController extends Controller
 {
-    public function __invoke()
-    {
-        $page = Page::slug('home')->firstOrNew();
+	public function __invoke()
+	{
+		$page = Page::slug('home')->firstOrNew();
 
-        $topics = Topic::with(['activeTags.mentors' => function ($q) {
-            $q->status()->whereNotNull('activated_at');
-        }, 'activeTags.mentors.profilePicture'])->active()->orderBy('title', 'ASC')->get();
+		$topics = Topic::with(['activeTags.mentors' => function ($q) {
+			$q->status()->whereNotNull('activated_at');
+		}, 'activeTags.mentors.profilePicture'])->active()->orderBy('title', 'ASC')->get();
 
-        return Inertia::render('Homepage', ['page' => $page, 'topics' => $topics]);
-    }
+		$posts = Post::with('categories')->latest()->published()->limit(5)->get();
+
+		return Inertia::render('Homepage', ['page' => $page, 'topics' => $topics, 'latestPosts' => PostResource::collection($posts)]);
+	}
 }
