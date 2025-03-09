08342d7efded9f77659bb60882f73f77e5353ee7
Abid Raza <ari.abid@gmail.com>
2025-03-09 19:50:38 +0530

    Fix page layout

--- resources/js/Layouts/PageLayout.jsx
+++ resources/js/Layouts/PageLayout.jsx
@@ -4,6 +4,8 @@ import Footer from "./Footer";
 import { Head, usePage } from "@inertiajs/react";
 import { Toaster } from "@/shadcn/ui/sonner";
 import { toast } from "sonner";
+import ShadcnProvider from "./shadcn-provider";
+import BlankLayout from "./blank-layout";
 const AdminGlobalHeader = React.lazy(() =>
     import("@/Components/Admin/AdminGlobalHeader")
 );
@@ -29,18 +31,9 @@ export default function PageLayout({ children, title, metaDescription }) {
         }
     }, [flash]);
     return (
-        <>
-            <Head title={title}>
-                <meta name="description" content={metaDescription} />
-            </Head>
-            {/* {auth &&
-                auth.user &&
-                auth.userRoles &&
-                auth.userRoles.includes("admin") && <AdminGlobalHeader />} */}
+        <BlankLayout title={title} metaDescription={metaDescription}>
             <Header />
             <div className="content">{children}</div>
-            <Toaster position="top-right" richColors />
-            <Footer />
-        </>
+        </BlankLayout>
     );
 }
