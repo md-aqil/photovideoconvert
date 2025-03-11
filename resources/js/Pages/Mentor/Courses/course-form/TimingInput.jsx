bba4bb445fde8aaa86f9480fca33c822c5648113
Abid Raza <ari.abid@gmail.com>
2025-03-12 04:09:43 +0530

    Add dialog footer and description

--- resources/js/Pages/Mentor/Courses/course-form/TimingInput.jsx
+++ resources/js/Pages/Mentor/Courses/course-form/TimingInput.jsx
@@ -11,8 +11,10 @@ import { CardHeader, CardTitle } from "@/shadcn/ui/card";
 import { addDays, format } from "date-fns";
 import {
     Dialog,
+    DialogClose,
     DialogContent,
     DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
@@ -95,183 +97,179 @@ const RecurringSlotsDialog = ({ onAdd, open, onOpenChange }) => {
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>Add Recurring Slots</DialogTitle>
-                    <DialogDescription className="space-y-4 pt-4">
-                        <div>
-                            <InputLabel>Select Date Range</InputLabel>
-                            <div className={cn("grid gap-2")}>
-                                <Popover>
-                                    <PopoverTrigger asChild>
-                                        <Button
-                                            id="date"
-                                            variant={"outline"}
-                                            className={cn(
-                                                "w-[300px] justify-start text-left font-normal",
-                                                !date && "text-muted-foreground"
-                                            )}
-                                        >
-                                            <CalendarIcon />
-                                            {date?.from ? (
-                                                date.to ? (
-                                                    <>
-                                                        {format(
-                                                            date.from,
-                                                            "LLL dd, y"
-                                                        )}{" "}
-                                                        -{" "}
-                                                        {format(
-                                                            date.to,
-                                                            "LLL dd, y"
-                                                        )}
-                                                    </>
-                                                ) : (
-                                                    format(
-                                                        date.from,
-                                                        "LLL dd, y"
-                                                    )
-                                                )
-                                            ) : (
-                                                <span>Pick a date</span>
-                                            )}
-                                        </Button>
-                                    </PopoverTrigger>
-                                    <PopoverContent
-                                        className="w-auto p-0"
-                                        align="start"
-                                    >
-                                        <Calendar
-                                            initialFocus
-                                            mode="range"
-                                            defaultMonth={date?.from}
-                                            selected={date}
-                                            onSelect={setDate}
-                                            numberOfMonths={2}
-                                        />
-                                    </PopoverContent>
-                                </Popover>
-                            </div>
-                        </div>
-                        <div className="flex gap-4 justify-between items-center">
-                            <div className="flex-1">
-                                <InputLabel>Every Week on</InputLabel>
-                                <Select
-                                    value={`${everyDay}`}
-                                    onValueChange={(v) => {
-                                        everyDaySet(v);
-                                        everyMonthSet(0);
-                                    }}
-                                >
-                                    <SelectTrigger className="w-full">
-                                        <SelectValue placeholder="Select Day" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectGroup>
-                                            <SelectItem value="-1">
-                                                Select Day
-                                            </SelectItem>
-                                            <SelectItem value="0">
-                                                Every Sunday
-                                            </SelectItem>
-                                            <SelectItem value="1">
-                                                Every Monday
-                                            </SelectItem>
-                                            <SelectItem value="2">
-                                                Every Tuesday
-                                            </SelectItem>
-                                            <SelectItem value="3">
-                                                Every Wednesday
-                                            </SelectItem>
-                                            <SelectItem value="4">
-                                                Every Thursday
-                                            </SelectItem>
-                                            <SelectItem value="5">
-                                                Every Friday
-                                            </SelectItem>
-                                            <SelectItem value="6">
-                                                Every Saturday
-                                            </SelectItem>
-                                        </SelectGroup>
-                                    </SelectContent>
-                                </Select>
-                            </div>
-                            <div>Or</div>
-                            <div className="flex-1">
-                                <InputLabel>Every Month on</InputLabel>
-                                <Select
-                                    value={`${everyMonth}`}
-                                    onValueChange={(v) => {
-                                        everyMonthSet(v);
-                                        everyDaySet(-1);
-                                    }}
-                                >
-                                    <SelectTrigger className="w-full">
-                                        <SelectValue placeholder="Select Month Date" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectGroup>
-                                            <SelectItem value="0">
-                                                Select Month Date
-                                            </SelectItem>
-                                            {[...Array(31).keys()].map((i) => (
-                                                <SelectItem
-                                                    key={i}
-                                                    value={`${i + 1}`}
-                                                >
-                                                    {i + 1}
-                                                </SelectItem>
-                                            ))}
-                                        </SelectGroup>
-                                    </SelectContent>
-                                </Select>
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-2 gap-4">
-                            <div>
-                                <InputLabel
-                                    htmlFor="startTime"
-                                    value={"Start Time"}
-                                    isRequired
-                                />
-                                <Input
-                                    id="startTime"
-                                    name="startTime"
-                                    type="time"
-                                    value={`${startTime}`}
-                                    onChange={(e) => {
-                                        let timeFormat = e.target.value + ":00";
-                                        startTimeSet(timeFormat);
-                                    }}
-                                    placeholder="Start Time"
-                                    className="block w-full"
-                                />
-                            </div>
-                            <div>
-                                <InputLabel
-                                    htmlFor="endTime"
-                                    value={"End Time"}
-                                    isRequired
-                                />
-                                <Input
-                                    id="endTime"
-                                    name="endTime"
-                                    type="time"
-                                    value={`${endTime}`}
-                                    onChange={(e) => {
-                                        let timeFormat = e.target.value + ":00";
-                                        endTimeSet(timeFormat);
-                                    }}
-                                    placeholder="End Time"
-                                    className="block w-full"
-                                />
-                            </div>
-                        </div>
-                        <Button
-                            type="button"
-                            onClick={createSlotsByDateRangeAndDay}
-                        >
-                            Add
-                        </Button>
+                    <DialogDescription>
+                        Add slots for multiple days at once by selecting a date
+                        range and day of the week or month.
                     </DialogDescription>
                 </DialogHeader>
+                <div>
+                    <InputLabel>Select Date Range</InputLabel>
+                    <div className={cn("grid gap-2")}>
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <Button
+                                    id="date"
+                                    variant={"outline"}
+                                    className={cn(
+                                        "w-[300px] justify-start text-left font-normal",
+                                        !date && "text-muted-foreground"
+                                    )}
+                                >
+                                    <CalendarIcon />
+                                    {date?.from ? (
+                                        date.to ? (
+                                            <>
+                                                {format(date.from, "LLL dd, y")}{" "}
+                                                - {format(date.to, "LLL dd, y")}
+                                            </>
+                                        ) : (
+                                            format(date.from, "LLL dd, y")
+                                        )
+                                    ) : (
+                                        <span>Pick a date</span>
+                                    )}
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent
+                                className="w-auto p-0"
+                                align="start"
+                            >
+                                <Calendar
+                                    initialFocus
+                                    mode="range"
+                                    defaultMonth={date?.from}
+                                    selected={date}
+                                    onSelect={setDate}
+                                    numberOfMonths={2}
+                                />
+                            </PopoverContent>
+                        </Popover>
+                    </div>
+                </div>
+                <div className="flex gap-4 justify-between items-center">
+                    <div className="flex-1">
+                        <InputLabel>Every Week on</InputLabel>
+                        <Select
+                            value={`${everyDay}`}
+                            onValueChange={(v) => {
+                                everyDaySet(v);
+                                everyMonthSet(0);
+                            }}
+                        >
+                            <SelectTrigger className="w-full">
+                                <SelectValue placeholder="Select Day" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectItem value="-1">
+                                        Select Day
+                                    </SelectItem>
+                                    <SelectItem value="0">
+                                        Every Sunday
+                                    </SelectItem>
+                                    <SelectItem value="1">
+                                        Every Monday
+                                    </SelectItem>
+                                    <SelectItem value="2">
+                                        Every Tuesday
+                                    </SelectItem>
+                                    <SelectItem value="3">
+                                        Every Wednesday
+                                    </SelectItem>
+                                    <SelectItem value="4">
+                                        Every Thursday
+                                    </SelectItem>
+                                    <SelectItem value="5">
+                                        Every Friday
+                                    </SelectItem>
+                                    <SelectItem value="6">
+                                        Every Saturday
+                                    </SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <div>Or</div>
+                    <div className="flex-1">
+                        <InputLabel>Every Month on</InputLabel>
+                        <Select
+                            value={`${everyMonth}`}
+                            onValueChange={(v) => {
+                                everyMonthSet(v);
+                                everyDaySet(-1);
+                            }}
+                        >
+                            <SelectTrigger className="w-full">
+                                <SelectValue placeholder="Select Month Date" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectItem value="0">
+                                        Select Month Date
+                                    </SelectItem>
+                                    {[...Array(31).keys()].map((i) => (
+                                        <SelectItem key={i} value={`${i + 1}`}>
+                                            {i + 1}
+                                        </SelectItem>
+                                    ))}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                    <div>
+                        <InputLabel
+                            htmlFor="startTime"
+                            value={"Start Time"}
+                            isRequired
+                        />
+                        <Input
+                            id="startTime"
+                            name="startTime"
+                            type="time"
+                            value={`${startTime}`}
+                            onChange={(e) => {
+                                let timeFormat = e.target.value + ":00";
+                                startTimeSet(timeFormat);
+                            }}
+                            placeholder="Start Time"
+                            className="block w-full"
+                        />
+                    </div>
+                    <div>
+                        <InputLabel
+                            htmlFor="endTime"
+                            value={"End Time"}
+                            isRequired
+                        />
+                        <Input
+                            id="endTime"
+                            name="endTime"
+                            type="time"
+                            value={`${endTime}`}
+                            onChange={(e) => {
+                                let timeFormat = e.target.value + ":00";
+                                endTimeSet(timeFormat);
+                            }}
+                            placeholder="End Time"
+                            className="block w-full"
+                        />
+                    </div>
+                </div>
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button type="button" variant="secondary">
+                            Close
+                        </Button>
+                    </DialogClose>
+                    <Button
+                        type="button"
+                        onClick={createSlotsByDateRangeAndDay}
+                    >
+                        Add
+                    </Button>
+                </DialogFooter>
             </DialogContent>
         </Dialog>
     );
@@ -302,10 +300,11 @@ export default function TimingInput({ errors, data, setData, course }) {
                     <div className="flex items-center space-x-2">
                         <Button
                             type="button"
-                            size="icon"
                             onClick={() => openRecurringSlogDialogSet(true)}
+                            variant="outline"
                         >
-                            <CalendarIcon size={18} />
+                            <CalendarIcon size={18} className="mr-2" /> Add
+                            Recurring Slots
                         </Button>
                         <RecurringSlotsDialog
                             open={openRecurringSlogDialog}
