34b9e9f4bcfbc90396c80bcb3e8fb14dc95e2336
Gautam Kumar <gtmkmr2u@gmail.com>
2025-04-12 11:48:04 +0530

    Send related mentor in search page and link to course and mentor

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\CourseResource;
+use App\Http\Resources\MentorProfileResource;
 use App\Models\Course;
 use App\Models\Page;
 use Illuminate\Http\Request;
@@ -18,6 +19,26 @@ class SearchController extends Controller
 
 		$courses = Course::whereLike(['title', 'description'], $keyword)->latest()->get();
 
-		return Inertia::render('Search', ['courses' => CourseResource::collection($courses), 'keyword' => $keyword, 'page' => $page]);
+		$courses = Course::where('title', 'like', '%' . $keyword . '%')
+			->with([
+				'mentorProfile.profilePicture',
+				'price',
+				'featuredImage',
+				'bundleCourses',
+				'topics',
+				'tags',
+				'timings'
+			])
+			->get();
+		$relatedMentors = $courses->pluck('mentorProfile')->filter()->unique('id')->values();
+		return Inertia::render(
+			'Search',
+			[
+				'courses' => CourseResource::collection($courses),
+				'keyword' => $keyword,
+				'mentors' => MentorProfileResource::collection($relatedMentors),
+				'page' => $page
+			]
+		);
 	}
 }
--- resources/js/Pages/Search.jsx
+++ resources/js/Pages/Search.jsx
@@ -1,10 +1,18 @@
 import PageBanner from "@/Components/PageBanner";
 import PageLayout from "@/Layouts/PageLayout";
-import { Card, CardContent, CardHeader } from "@/shadcn/ui/card";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/shadcn/ui/card";
 import SearchForm from "@/Components/Search";
 import SectionWrapper from "@/Components/SectionWrapper";
+import { Link } from "@inertiajs/react";
+import { UserIcon } from "lucide-react";
 
-function Search({ courses, keyword }) {
+function Search({ courses, mentors }) {
     return (
         <>
             <PageBanner title={"Search"} />
@@ -14,8 +22,43 @@ function Search({ courses, keyword }) {
                 </div>
 
                 {courses.data.map((course) => (
-                    <Card key={course.id} className="mb-2">
-                        <CardHeader>{course.title}</CardHeader>
+                    <Card
+                        key={course.id}
+                        className="mb-2 group hover:shadow-md hover:bg-fomoLight-0 hover:bg-opacity-15 transition duration-150 ease-in-out"
+                    >
+                        <CardHeader>
+                            <CardTitle className="text-lg group-hover:underline cursor-pointer group-hover:text-blue-600 underline-offset-4 transition duration-150 ease-in-out">
+                                <Link
+                                    href={route(
+                                        "course.find-by-slug",
+                                        course.slug,
+                                    )}
+                                >
+                                    {course.title}
+                                </Link>
+                            </CardTitle>
+                            {/* {console.log(
+                                "🚀 ~ Search ~ course:",
+                                course.mentor_profile.unique_id,
+                            )} */}
+                            <CardDescription>
+                                <Link
+                                    href={route(
+                                        "mentor.find-by-id",
+                                        course.mentor_profile.unique_id,
+                                    )}
+                                    className="flex items-center gap-1"
+                                >
+                                    <UserIcon className="h-4 w-4" />
+                                    <span className="text-blue-500">
+                                        {course?.mentor_profile?.alias_name &&
+                                        course?.mentor_profile?.show_alias === 1
+                                            ? course?.mentor_profile?.alias_name
+                                            : course?.mentor_profile?.full_name}
+                                    </span>
+                                </Link>
+                            </CardDescription>
+                        </CardHeader>
                         <CardContent>{course.excerpt}</CardContent>
                     </Card>
                 ))}
