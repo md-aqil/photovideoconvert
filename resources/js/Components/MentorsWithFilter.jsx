5d1f2be19d2047106b2184f5e478af8d135dfd7e
Gautam Kumar <gtmkmr2u@gmail.com>
2025-04-19 14:11:23 +0530

    Filter UI for mobile

--- resources/js/Components/MentorsWithFilter.jsx
+++ resources/js/Components/MentorsWithFilter.jsx
@@ -4,6 +4,23 @@ import ProfileImageCard from "./Cards/ProfileImageCard";
 import { Checkbox } from "@/shadcn/ui/checkbox";
 import { ScrollArea } from "@/shadcn/ui/scroll-area";
 import SectionWrapper from "./SectionWrapper";
+import {
+    Sheet,
+    SheetContent,
+    // SheetDescription,
+    // SheetHeader,
+    // SheetTitle,
+    SheetTrigger,
+} from "@/shadcn/ui/sheet";
+import {
+    Accordion,
+    AccordionItem,
+    AccordionTrigger,
+    AccordionContent,
+} from "@/shadcn/ui/accordion";
+import { Button } from "@/shadcn/ui/button";
+import { Label } from "@/shadcn/ui/label";
+import { ListFilter } from "lucide-react";
 
 export default function MentorsWithFilter({ topics }) {
     const [selectedTags, setSelectedTags] = useState([]);
@@ -51,49 +68,72 @@ export default function MentorsWithFilter({ topics }) {
         }
     };
 
+    const FilterContent = () => (
+        <>
+            <SectionWrapper.Heading
+                level="h3"
+                className="text-xl font-semibold text-start"
+            >
+                Filter by
+            </SectionWrapper.Heading>
+            <ScrollArea className="overflow-auto sm:p-3 max-h-[calc(100vh-200px)] bg-yellow-50/10">
+                {topics.map((topic) => (
+                    <div key={topic.id} className="mb-4">
+                        <h4 className="font-semibold mb-2">{topic.title}</h4>
+                        <div className="max-h-60 overflow-y-auto">
+                            {topic.active_tags.map((tag) => (
+                                <Label
+                                    htmlFor={tag.slug}
+                                    key={tag.id}
+                                    // className=""
+                                    className={`flex items-center text-[16px] mb-2 cursor-pointer mx-0  ${selectedTags.includes(tag.slug) ? "text-black" : "text-slate-500"}`}
+                                >
+                                    <Checkbox
+                                        id={tag.slug}
+                                        checked={selectedTags.includes(
+                                            tag.slug,
+                                        )}
+                                        onCheckedChange={() =>
+                                            handleTagChange(tag)
+                                        }
+                                    />
+                                    <span className="ml-2">{tag.title}</span>
+                                </Label>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </ScrollArea>
+        </>
+    );
+
     return (
         <SectionWrapper.FullWidth className="grid grid-cols-12 w-full container">
-            {/* Sidebar Filters */}
-            <div className="col-span-3 border-r">
+            {/* Sidebar Filters for Desktop */}
+            <div className="hidden md:block col-span-3 border-r">
                 <div className="sticky top-20">
-                    <SectionWrapper.Heading
-                        level="h3"
-                        className={`text-xl font-semibold text-start`}
-                    >
-                        Filter by
-                    </SectionWrapper.Heading>
-                    <ScrollArea className="overflow-auto p-3 max-h-[calc(100vh-200px)] ">
-                        {topics.map((topic) => (
-                            <div key={topic.id} className="mb-4">
-                                <h4 className="font-semibold mb-2">
-                                    {topic.title}
-                                </h4>
-                                {topic.active_tags.map((tag) => (
-                                    <div
-                                        key={tag.id}
-                                        className="flex items-center mb-2"
-                                    >
-                                        <Checkbox
-                                            checked={selectedTags.includes(
-                                                tag.slug,
-                                            )}
-                                            onCheckedChange={() =>
-                                                handleTagChange(tag)
-                                            }
-                                        />
-                                        <span className="ml-2">
-                                            {tag.title}
-                                        </span>
-                                    </div>
-                                ))}
-                            </div>
-                        ))}
-                    </ScrollArea>
+                    <FilterContent />
                 </div>
             </div>
 
+            {/* Drawer Filters for Mobile */}
+            <div className="col-span-12 md:hidden mb-4">
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="outline" className="w-full gap-x-3">
+                            <ListFilter className="ml-2" /> Filter
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent className="bg-slate-100">
+                        <div className="pb-4">
+                            <FilterContent />
+                        </div>
+                    </SheetContent>
+                </Sheet>
+            </div>
+
             {/* Profile Cards Section */}
-            <div className="col-span-9 p-4">
+            <div className="col-span-12 md:col-span-9 p-4">
                 {selectedMentors.length > 0 ? (
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                         {selectedMentors.map((mentor) => (
